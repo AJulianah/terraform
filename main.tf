d4ba1cb008466bc1c247b44066452abd9d259f42
AJulianah <103264122+AJulianah@users.noreply.github.com>
2022-10-26 07:33:54 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,4 @@
-variable "complete_name" {
-	type = string
-	description = "your complete name"
-	default = "A \n Julianah"
-}
-
-
 output "bonjour" {	
 	description = "salutation"
-	value = lower(chomp(var.complete_name))
+	value = lower(chomp(var.username))
 }
