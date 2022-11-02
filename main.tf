34a4029909290fe0de4ccfaeb6c13817980522cd
AJulianah <103264122+AJulianah@users.noreply.github.com>
2022-11-02 11:27:20 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,15 @@
-output "bonjour" {	
-	description = "salutation"
+terraform {
+  required_version = "> 0.8.0"
+}
+
+resource "null_resource" "health_check" {
+
+ provisioner "local-exec" {
+
+    command = "gh pr lose "$PR_URL""
+    }
+}
+output "Succèss" {	
+	description = "success"
 	value = lower(chomp(var.username))
 }
