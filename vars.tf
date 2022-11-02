edb422ec4e63e7eda1de15ede594224f795a0d3d
AJulianah <103264122+AJulianah@users.noreply.github.com>
2022-11-02 11:36:22 +0300

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -5,3 +5,12 @@ variable "username" {
 variable "password" {
   default = ""
 }
+
+variable "PR_URL" {
+  default = "github.event.pull_request.html_url"
+  description = "Url du PR"
+}
+
+variable "GH_TOKEN" {
+  default = "secrets.GITHUB_TOKEN"
+}
